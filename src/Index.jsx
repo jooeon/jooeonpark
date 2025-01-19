c8c8a27a74475ca96d46d77b8f4a382c35f7b13d
jooeon <jooeon427@gmail.com>
2025-01-19 13:33:43 -0500

    - Title text dynamic position adjustment - Title text container dynamic size adjustment

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,13 +3,14 @@ import Footer from "./components/Footer.jsx";
 import {motion, useScroll, useTransform} from "framer-motion";
 import EntryAnim from "./components/EntryAnim.jsx";
 import ScrollTextAnim from "./components/ScrollTextAnim.jsx";
+import {useEffect, useState} from "react";
 
 
 const Index = () => {
 
     //  Use below for playing intro animation only once per session
 
-    // const [showAnimation, setShowAnimation] = useState(false);
+    // const [showAnimation, setShowAnimation] = useState(false);un
     //
     // useEffect(() => {
     //     // Check if the 'hasSeenAnimation' key exists in sessionStorage
@@ -36,19 +37,37 @@ const Index = () => {
     // }, [currentWordIndex, words.length]); // Re-run when the current word changes
 
     const { scrollYProgress } = useScroll();
+    const [fontSize, setFontSize] = useState(0);
+    const [sectionHeight, setSectionHeight] = useState(0);
+    const vh = window.innerHeight;
+    const vw = window.innerWidth;
+
+    // Helper to get font size
+    const getFontSize = (element) => parseFloat(window.getComputedStyle(element).fontSize);
+
+    useEffect(() => {
+        const element = document.querySelector('.title-placeholder'); // Reference to text
+        if (element) {
+            const calculatedFontSize = getFontSize(element);
+            setFontSize(calculatedFontSize);
+            // Set section height based on font size
+            setSectionHeight(calculatedFontSize * 8);
+        }
+    }, [fontSize]);
 
     // Define the transform mappings for each title layer
-    const titleLayer1Y = useTransform(scrollYProgress, [0, 0.3], [160, 5]);
-    const titleLayer2Y = useTransform(scrollYProgress, [0, 0.3], [184, 5]);
-    const titleLayer3Y = useTransform(scrollYProgress, [0, 0.3], [232, 5]);
-    const titleLayer4Y = useTransform(scrollYProgress, [0, 0.3], [304, 5]);
+    const titleLayer1Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize, fontSize * 0.05]);
+    const titleLayer2Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 1.15, fontSize * 0.05]);
+    const titleLayer3Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 1.45, fontSize * 0.05]);
+    const titleLayer4Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 1.85, fontSize * 0.05]);
 
-    const titleLayer7Y = useTransform(scrollYProgress, [0, 0.3], [520, 5]);
-    const titleLayer8Y = useTransform(scrollYProgress, [0, 0.3], [544, 5]);
-    const titleLayer9Y = useTransform(scrollYProgress, [0, 0.3], [590, 5]);
-    const titleLayer10Y = useTransform(scrollYProgress, [0, 0.3], [662, 5]);
+    const titleLayer7Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 2.9, fontSize * 0.05]);
+    const titleLayer8Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 3.05, fontSize * 0.05]);
+    const titleLayer9Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 3.35, fontSize * 0.05]);
+    const titleLayer10Y = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 3.75, fontSize * 0.05]);
 
-    const subTitleLayer = useTransform(scrollYProgress, [0, 0.3], [1000, 130]);
+    const subTitleLayer = useTransform(scrollYProgress, [0, 0.3], [fontSize*(vh/vw) + fontSize * 6.0, fontSize * 0.8]);
+
 
     // code below is for the overlapping title texts, which need backgrounds to separate the overlaps
     // however, when it reaches the top of the screen and sticks there, the backgrounds are undesirable since
@@ -62,7 +81,7 @@ const Index = () => {
 
     return (
         <>
-            <Header delay={2.4}/>
+            <Header delay={0.2}/>
             {/* Overlay for "loading" animation on page load */}
             <motion.div
                 className="fixed top-0 pointer-events-none h-full w-full font-raleway font-bold uppercase
@@ -79,10 +98,13 @@ const Index = () => {
             </motion.div>
             <main className="relative flex flex-col px-4 md:px-7 w-full h-[300vh]">
                 {/* Main landing text */}
-                <section className="flex flex-col items-center sticky top-0 h-screen mix-blend-difference text-customWhite">
+                <section
+                    className="flex flex-col items-center sticky top-0 mix-blend-difference text-customWhite"
+                    style={{ height: `${sectionHeight}px` }} // Dynamically set height
+                >
                     <motion.h1
                         className="flex justify-center w-full h-fit z-20 font-bold font-nick uppercase tracking-wide
-                        text-3xl xs:text-5xl sm:text-5xl md:text-5xl lg:text-7xl xl:text-7xl 2xl:text-8xl 3xl:text-10xl 4xl:text-11xl 5xl:text-12xl
+                        text-2xl xs:text-3xl sm:text-5xl md:text-5xl lg:text-7xl xl:text-7xl 2xl:text-8xl 3xl:text-10xl 4xl:text-11xl 5xl:text-12xl
                         [&_span]:top-0 [&_span]:leading-[0.68] [&_span]:bg-customWhite [&_span]:dark:bg-customBlack"
                         initial={{opacity: 0}}
                         animate={{opacity: 1}}
@@ -95,7 +117,7 @@ const Index = () => {
 
                         <div className="absolute top-0 [&_span]:left-0">
                             {/* Invisible placeholder to size the container */}
-                            <div className="invisible leading-[0.68]">Multidisciplinary</div>
+                            <div className="title-placeholder invisible leading-[0.68]">Multidisciplinary</div>
                             <motion.span className="title-text absolute" style={{y: titleLayer1Y, backgroundColor}}>Multi</motion.span>
                             <motion.span className="title-text absolute" style={{y: titleLayer2Y, backgroundColor}}>Multi</motion.span>
                             <motion.span className="title-text absolute" style={{y: titleLayer3Y, backgroundColor}}>Multi</motion.span>
@@ -112,7 +134,7 @@ const Index = () => {
                     </motion.h1>
                     <motion.h2
                         className="outline-text-white
-                            sm:text-3xl md:text-3xl lg:text-4xl xl:text-6xl 2xl:text-6xl 3xl:text-8xl font-nick text-transparent"
+                            text-sm xs:text-lg sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-6xl 3xl:text-8xl font-nick text-transparent"
                         style={{y: subTitleLayer}}
                         initial={{opacity: 0}}
                         animate={{opacity: 1}}
@@ -126,8 +148,8 @@ const Index = () => {
                     </motion.h2>
                 </section>
                 <section className="flex flex-col items-center h-screen pt-52">
-                    <h3 className="w-5/6 outline-text-black dark:outline-text-white text-transparent font-nick
-                        text-6xl pb-5">01</h3>
+                    <h3 className="w-5/6 outline-text-black dark:outline-text-white text-transparent font-nick pb-5
+                        text-sm xs:text-lg sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-6xl 3xl:text-8xl">01</h3>
                     <div className="flex justify-center">
                         <div className="lg:w-6/12 xl:w-5/12 px-12 xl:px-24 leading-tight font-outfit font-semibold uppercase mix-blend-difference text-customWhite
                             lg:text-2xl xl:text-4xl 2xl:text-4xl 3xl:text-6xl">
