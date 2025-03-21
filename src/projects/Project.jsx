add0cdc8b20e80907368e946d0878f6ab66fc6d9
Joo Eon <jooeon427@gmail.com>
2025-03-20 22:40:34 -0400

    - Adjusted onload animation timing for images and videos on artwork detail page (Project.jsx)

--- src/projects/Project.jsx
+++ src/projects/Project.jsx
@@ -60,7 +60,7 @@ const Project = () => {
                                         initial={{opacity: 0, y: 40}}
                                         animate={{opacity: 1, y: 0}}
                                         transition={{
-                                            duration: 0.5,
+                                            duration: 0.3,
                                             delay: 1.0,
                                             ease: "easeOut",
                                         }}
@@ -114,7 +114,7 @@ const Project = () => {
                                         initial={{opacity: 0, y: 40}}
                                         animate={{opacity: 1, y: 0}}
                                         transition={{
-                                            duration: 0.5,
+                                            duration: 0.3,
                                             delay: 1.0,
                                             ease: "easeOut",
                                         }}
