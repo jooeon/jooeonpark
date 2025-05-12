adb1a48dc4f02ef36cf5972f6c8862a7b78d10c8
Joo Eon <jooeon427@gmail.com>
2025-05-11 23:24:23 -0400

    - Images in project detail page are now clickable and show an overlay

--- src/components/unused/Overlay.jsx
+++ src/components/unused/Overlay.jsx
@@ -1,13 +1,10 @@
 import{ useEffect } from "react";
 import { motion } from "framer-motion";
-import PropTypes from "prop-types"; // Import PropTypes
+import PropTypes from "prop-types";
 
 const Overlay = ({
                      images,
-                     caption,
                      currentIndex,
-                     prevImage,
-                     nextImage,
                      closeOverlay,
                  }) => {
     // Handle ESC key to close the overlay
@@ -21,7 +18,6 @@ const Overlay = ({
         return () => window.removeEventListener("keydown", handleEsc);
     }, [closeOverlay]);
 
-    // Framer Motion variants for the backdrop and content
     const backdropVariants = {
         hidden: { opacity: 0 },
         visible: { opacity: 1 },
@@ -36,7 +32,7 @@ const Overlay = ({
 
     return (
         <motion.div
-            className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+            className="fixed inset-0 bg-black bg-opacity-75 flex md:gap-5 lg:gap-7 3xl:gap-14 4xl:gap-20 items-center justify-center z-30"
             variants={backdropVariants}
             initial="hidden"
             animate="visible"
@@ -46,7 +42,7 @@ const Overlay = ({
             role="dialog"
         >
             <motion.div
-                className="overflow-hidden relative top-1.5 max-w-90vw w-fit mx-4"
+                className="relative w-fit"
                 variants={contentVariants}
                 initial="hidden"
                 animate="visible"
@@ -58,52 +54,15 @@ const Overlay = ({
                 <img
                     src={images[currentIndex]}
                     alt={`Image ${currentIndex + 1}`}
-                    className="max-w-90vw max-h-90vh"
+                    className="max-w-[80vw] max-h-[90vh]"
                     key={currentIndex} // Key to trigger animation on change
                 />
 
                 {/* Caption */}
-                <div className="relative p-2">
-                    <h2
-                        className="text-base"
-                        dangerouslySetInnerHTML={{ __html: caption }}
-                    ></h2>
-                    <p className="absolute top-2 right-2">{currentIndex + 1}/{images.length}</p>
-                </div>
+                {/*<div className="text-right pt-1">*/}
+                {/*  <p className="">{currentIndex + 1}/{images.length}</p>*/}
+                {/*</div>*/}
 
-                {/* Navigation Buttons */}
-                {images.length > 1 && (
-                    <>
-                        <motion.button
-                            onClick={prevImage}
-                            className="fixed top-1/2 left-5 text-white p-2 text-5xl"
-                            aria-label="Previous Image"
-                            initial={{opacity: 0}}
-                            animate={{opacity: 1}}
-                            transition={{
-                                duration: 0.6,
-                                delay: 0.1,
-                                ease: "easeIn"
-                            }}
-                        >
-                            ❮
-                        </motion.button>
-                        <motion.button
-                            onClick={nextImage}
-                            className="fixed top-1/2 right-5 text-white p-2 text-5xl"
-                            aria-label="Next Image"
-                            initial={{opacity: 0}}
-                            animate={{opacity: 1}}
-                            transition={{
-                                duration: 0.6,
-                                delay: 0.1,
-                                ease: "easeIn"
-                            }}
-                        >
-                            ❯
-                        </motion.button>
-                    </>
-                )}
             </motion.div>
         </motion.div>
     );
@@ -112,10 +71,7 @@ const Overlay = ({
 // Define propTypes
 Overlay.propTypes = {
     images: PropTypes.arrayOf(PropTypes.string).isRequired, // Array of image URLs
-    caption: PropTypes.string.isRequired, // Caption string
     currentIndex: PropTypes.number.isRequired, // Current image index
-    prevImage: PropTypes.func.isRequired, // Function to go to previous image
-    nextImage: PropTypes.func.isRequired, // Function to go to next image
     closeOverlay: PropTypes.func.isRequired, // Function to close the overlay
 };
 
