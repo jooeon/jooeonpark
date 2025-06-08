8b1778866dd0b3a042845f9c08377e5de66b209b
Joo Eon <jooeon427@gmail.com>
2025-06-08 17:13:52 -0400

    - Adjusted navbar and footer padding - layered scroll title disappears when scroll hits bottom of page (index page)

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <footer className="z-30">
         <motion.div
-            className="flex items-center justify-between w-full p-3 md:p-5 text-3xs sm:text-xs md:text-sm 4xl:text-2xl 6xl:text-4xl uppercase font-neueHaasGrotesk font-extrabold tracking-wider"
+            className="flex items-center justify-between w-full p-3 md:p-5 xl:px-5 xl:py-6 3xl:px-7 3xl:py-8 4xl:px-10 4xl:py-10 7xl:px-14 7xl:py-16
+                text-3xs sm:text-xs md:text-sm 4xl:text-2xl 6xl:text-4xl lowercase font-neueHaasGrotesk font-extrabold tracking-wider"
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             transition={{
@@ -14,7 +15,10 @@ const Footer = () => {
                 ease: "linear"
             }}
         >
-            <p>&copy;{new Date().getFullYear()}. Joo Eon Park</p>
+            <p className="flex gap-8 md:gap-10 xl:gap-16 3xl:gap-20 4xl:gap-24 6xl:gap-36">
+                <span>&copy;{new Date().getFullYear()}</span>
+                <span>Joo Eon Park</span>
+            </p>
             <div className="flex items-center"><ThemeToggle /></div>
         </motion.div>
     </footer>
