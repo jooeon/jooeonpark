74cb84be2a42deb500f0a9fffe4220cbfd61bd07
Joo Eon <jooeon427@gmail.com>
2025-07-03 23:41:33 -0400

    - Modified title animation

--- src/pages/RandomImageGenerator.jsx
+++ src/pages/RandomImageGenerator.jsx
@@ -2,6 +2,7 @@ import RandomImageHalftone from "../components/RandomImageHalftone.jsx";
 import TitleText from "../components/templates/TitleText.jsx";
 import Header from "../components/templates/Header.jsx";
 import Footer from "../components/templates/Footer.jsx";
+import {motion} from "framer-motion";
 
 const RandomImageGenerator = () => {
     return (
@@ -9,9 +10,18 @@ const RandomImageGenerator = () => {
             <Header/>
             <div className="flex items-baseline gap-[3vw] xl:gap-[2vw]">
                 <TitleText phrase={"R.I.G."}/>
-                <p className="leading-none text-[2vh] xl:text-[1vw] font-neueHaasGrotesk font-semibold lowercase">
+                <motion.p
+                    className="leading-none text-[2vh] xl:text-[1vw] font-neueHaasGrotesk font-semibold lowercase"
+                    initial={{ opacity: 0, y: 15 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        duration: 0.35,
+                        delay: 1.3,
+                        ease: "easeOut",
+                    }}
+                >
                     /Random Image Generator
-                </p>
+                </motion.p>
             </div>
             <section className="my-[5vh] lg:my-[6vh] xl:my-[6vw] p-3 md:p-3.5 xl:p-3.5 4xl:p-8 7xl:p-10">
                 <RandomImageHalftone/>
